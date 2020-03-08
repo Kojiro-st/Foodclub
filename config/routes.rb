8b79794583b069bbd8899c4a212f5748a8253bc7
Kojiro-st <tenmadetobe5a@gmail.com>
2020-03-08 14:02:55 +0900

    7th(top-page-view-left)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
+  resources :admins, only: [:edit, :update]
   devise_for :users
-  get 'messages/index'
+  # get 'messages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "messages#index"
+  resources :users, only: [:edit, :update]
 end
